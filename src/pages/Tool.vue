6c523e3b0fc9e87a86d0eba37191d36b87f50f9b
王邸 <e110110eee@gmail.com>
2023-04-06 14:49:36 +0800

    add cal arc add commeric task best way

--- src/pages/Tool.vue
+++ src/pages/Tool.vue
@@ -4,20 +4,52 @@
         <div class="col-12">
             <card type="table-responsive text-left">
                 <template slot="header">
-                    <template v-if="!isRTL">
+                    <template>
                         <h1 class="title d-inline">常用链接</h1>
                     </template>
                 </template>
-                <div class="table-full-width " v-for="url in urltool" :key="url">
+                <div class="table-full-width " v-for="(url, index) in urltool" :key="index">
                     <h4><span class="url-size">{{url.text}}:</span><a :href="url.link">{{url.link}}</a></h4>
                 </div>
             </card>
         </div>
 
-        <div class="col-12" v-for="tool in imgTool" :key="tool">
+        <div class="col-12">
             <card type="table-responsive text-left">
                 <template slot="header">
-                    <template v-if="!isRTL">
+                    <template>
+                        <h1 class="title d-inline">神秘球计算</h1>
+                    </template>
+                </template>
+                <div class="row" v-for="(ball,index) in balls" :key="index">
+                    <div class="col-md-2 px-md-1 text-left">
+                        <fieldset disabled>
+                            <base-input label="岛名字" v-model="balls[index].name" disable></base-input>
+                        </fieldset>
+                    </div>
+                    <div class="col-md-2 px-md-1 text-left">
+                        <base-input label="每日获取量" v-model="balls[index].daily"></base-input>
+                    </div>
+                    <div class="col-md-2 px-md-1 text-left">
+                        <base-input label="当前等级" placeholder="当前等级" v-model="balls[index].nowLevel"></base-input>
+                    </div>
+                    <div class="col-md-2 px-md-1 text-left">
+                        <base-input label="当前等级的球数" placeholder="当前等级的球数" v-model="balls[index].nowCount"></base-input>
+                    </div>
+                    <div class="col-md-2 px-md-1 text-left">
+                        <base-input label="还需要多少天" placeholder="还需要多少天" v-model="balls[index].needDay"></base-input>
+                    </div>
+                </div>
+                <div class="row">
+                    <base-button type="success" fill @click="caculatorDay">计算</base-button>
+                </div>
+            </card>
+        </div>
+
+        <div class="col-12" v-for="tool in imgTool" :key="tool.text">
+            <card type="table-responsive text-left">
+                <template slot="header">
+                    <template>
                         <h1 class="title d-inline">{{tool.text}}</h1>
                     </template>
                 </template>
@@ -32,15 +64,20 @@
 
 <script>
 import {
-    Card
+    Card,
+    BaseInput
 } from "@/components/index";
+
 import TaskList from "./Dashboard/TaskList";
 import BaseTable from "@/components/BaseTable";
+import BaseButton from "@/components/BaseButton";
 
 export default {
     components: {
         Card,
         BaseTable,
+        BaseInput,
+        BaseButton,
         TaskList,
 
     },
@@ -63,31 +100,104 @@ export default {
 
             ],
             imgTool: [{
-                text: "完美核心",
-                url: require("@/assets/tool/core.jpg"),
-                width: "80%"
-            },
-            {
-                text: "内在概率表",
-                url: require("@/assets/tool/neizai.png"),
-                width: "60%"
-            },
-            {
-                text: "经验倍增计算表",
-                url: require("@/assets/tool/exp1.jpg"),
-                width: "80%"
-            },
-            {
-                text: "经验拉满升级攻略",
-                url: require("@/assets/tool/exp2.png"),
-                width: "80%"
-            },
-            
+                    text: "完美核心",
+                    url: require("@/assets/tool/core.jpg"),
+                    width: "80%"
+                },
+                {
+                    text: "航海单人",
+                    url: require("@/assets/tool/danhang.jpg"),
+                    width: "80%"
+                },
+                {
+                    text: "内在概率表",
+                    url: require("@/assets/tool/neizai.png"),
+                    width: "60%"
+                },
+                {
+                    text: "经验倍增计算表",
+                    url: require("@/assets/tool/exp1.jpg"),
+                    width: "80%"
+                },
+                {
+                    text: "经验拉满升级攻略",
+                    url: require("@/assets/tool/exp2.png"),
+                    width: "80%"
+                },
+
             ],
+            balls: [{
+                    name: "消逝球",
+                    daily: 14,
+                    nowLevel: 1,
+                    nowCount: 1,
+                    needDay: ""
+                },
+                {
+                    name: "啾啾球",
+                    daily: 19,
+                    nowLevel: 1,
+                    nowCount: 1,
+                    needDay: ""
+                },
+                {
+                    name: "拉克兰",
+                    daily: 12,
+                    nowLevel: 1,
+                    nowCount: 1,
+                    needDay: ""
+                },
+                {
+                    name: "阿尔球",
+                    daily: 12,
+                    nowLevel: 1,
+                    nowCount: 1,
+                    needDay: ""
+                },
+                {
+                    name: "魔拉球",
+                    daily: 8,
+                    nowLevel: 1,
+                    nowCount: 1,
+                    needDay: ""
+                },
+                {
+                    name: "艾斯球",
+                    daily: 8,
+                    nowLevel: 1,
+                    nowCount: 1,
+                    needDay: ""
+                }
+            ]
 
         }
     },
-    methods: {},
+    methods: {
+        // （总球数 - 当前球数）/ 每天获取 = 总天数
+        caculatorDay() {
+            var balls = this.balls;
+            for (let index = 0; index < balls.length; index++) {
+                const element = balls[index];
+                if (element.nowLevel >= 20 || element.nowCount > 20 * 20 + 11) {
+                    element.needDay = "参数错误";
+                    continue;
+                }
+                let nowTotalBall = this.getNowBall(element.nowLevel) + element.nowCount;
+                let needBall = 2679 - nowTotalBall;
+                var needDay = Math.ceil(needBall / element.daily)
+                element.needDay = needDay;
+            }
+
+        },
+        getNowBall(nowLevel) {
+            let haveFullLevel = nowLevel - 1;
+            let nowBall = 0;
+            for (let index = haveFullLevel; index > 0; index--) {
+                nowBall += index * index + 11;
+            }
+            return nowBall;
+        }
+    },
     created() {},
     mounted() {}
 }
